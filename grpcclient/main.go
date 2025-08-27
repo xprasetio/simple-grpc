3883ccf63234fa3a3af433459ad5d831b7414ade
eko-prasetio <caedablerg@gmail.com>
2025-08-27 21:08:18 +0700

    client streaming grpc

--- grpcclient/main.go
+++ grpcclient/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"grpc/pb/user"
+	"grpc/pb/chat"
 	"log"
 
 	"google.golang.org/grpc"
@@ -10,23 +10,50 @@ import (
 )
 
 func main() {
-	cliendConn, err := grpc.NewClient("localhost:8082", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	clientConn, err := grpc.NewClient("localhost:8082", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal("failed to connect: ", err)
 	}
-	userClient := user.NewUserServiceClient(cliendConn)
-	response, err := userClient.CreateUser(context.Background(), &user.User{
-		Id:      1,
-		Age:     18,
-		Balance: 14000,
-		Address: &user.Address{
-			Province: "shanghai",
-			City:     "shanghai",
-			Country:  "china",
-		},
+	// userClient := user.NewUserServiceClient(cliendConn)
+	// response, err := userClient.CreateUser(context.Background(), &user.User{
+	// 	Id:      1,
+	// 	Age:     18,
+	// 	Balance: 14000,
+	// 	Address: &user.Address{
+	// 		Province: "shanghai",
+	// 		City:     "shanghai",
+	// 		Country:  "china",
+	// 	},
+	// })
+	// if err != nil {
+	// 	log.Fatal("failed to create user: ", err)
+	// }
+	// log.Println("create user response: ", response.Message)
+
+	chatClient := chat.NewChatServiceClient(clientConn)
+	stream, err := chatClient.SendMessage(context.Background())
+	if err != nil {
+		log.Fatal("failed to send message: ", err)
+	}
+	err =stream.Send(&chat.ChatMessage{
+		UserId:  1,
+		Content: "hello from client",
 	})
 	if err != nil {
-		log.Fatal("failed to create user: ", err)
+		log.Fatal("failed to send via stream: ", err)
 	}
-	log.Println("create user response: ", response.Message)
+	err =stream.Send(&chat.ChatMessage{
+		UserId:  2,
+		Content: "hello from again",
+	})
+	if err != nil {
+		log.Fatal("failed to send via stream: ", err)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal("failed to receive message: ", err)
+
+	}
+	log.Println("connection closed: ", res.Content)
 }
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
+	"grpc/pb/chat"
 	"grpc/pb/user"
+	"io"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type userService struct {
@@ -21,6 +26,33 @@ func (us *userService) CreateUser(ctx context.Context, userReq *user.User) (*use
 	}, nil
 }
 
+type chatService struct {
+	chat.UnimplementedChatServiceServer
+}
+
+func (cs *chatService) SendMessage(stream grpc.ClientStreamingServer[chat.ChatMessage, chat.ChatMessage]) error {
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return status.Errorf(codes.Unknown, "failed to receive message: %v", err)
+		}
+		log.Printf("Receive message from user %d: %s", res.UserId, res.Content)
+	}
+	return stream.SendAndClose(&chat.ChatMessage{
+		Content: "Thanks for your message!",
+	})
+}
+
+// func (UnimplementedChatServiceServer) ReceiveMessage(*ReceiveMessageRequest, grpc.ServerStreamingServer[ChatMessage]) error {
+// 	return status.Errorf(codes.Unimplemented, "method ReceiveMessage not implemented")
+// }
+// func (UnimplementedChatServiceServer) Chat(grpc.BidiStreamingServer[ChatMessage, ChatMessage]) error {
+// 	return status.Errorf(codes.Unimplemented, "method Chat not implemented")
+// }
+
 func main() {
 	list, err := net.Listen("tcp", ":8082")
 	if err != nil {
@@ -29,6 +61,8 @@ func main() {
 	serv := grpc.NewServer()
 
 	user.RegisterUserServiceServer(serv, &userService{})
+	chat.RegisterChatServiceServer(serv, &chatService{})
+
 	log.Println("gRPC server is running on port 8082")
 
 	reflection.Register(serv) // mentok di development jangan sampai production
